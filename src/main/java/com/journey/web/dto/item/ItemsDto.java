67f32cd79ac7f470630d417b2e89720cc1addad4
Koojiny <msj09252@gmail.com>
2022-09-29 16:44:31 +0900

    [Fix/Domain] Items Dto수정

--- src/main/java/com/journey/web/dto/item/ItemsDto.java
+++ src/main/java/com/journey/web/dto/item/ItemsDto.java
@@ -10,7 +10,7 @@ public class ItemsDto {
     private String name;
     private int price;
     private String company;     // 제조사
-    private String type;        // 주종(탁주 T, 소주 S, 와인 W, 청주 C)
+    private String soolType;    // 주종(탁주 T, 소주 S, 와인 W, 청주 C)
     private int stockQuantity;  // 재고
     private String image_url;
 }
