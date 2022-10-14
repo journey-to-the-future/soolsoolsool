8b6ccb0fe8f5eeee84459b37d478368615ea1e08
Koojiny <msj09252@gmail.com>
2022-10-14 11:46:08 +0900

    [Fix/Controller] fix CORS configuration

--- src/main/java/com/journey/web/config/CorsFilter2.java
+++ src/main/java/com/journey/web/config/CorsFilter2.java
@@ -1,11 +1,16 @@
 package com.journey.web.config;
 
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter2 implements Filter {
 
     @Override
