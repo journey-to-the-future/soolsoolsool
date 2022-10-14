8b90c19b776f21e3c75a68b36e98454d89007e4d
Koojiny <msj09252@gmail.com>
2022-10-14 11:36:51 +0900

    [Fix/Controller] add CrossOrigin

--- src/main/java/com/journey/web/controller/ItemController.java
+++ src/main/java/com/journey/web/controller/ItemController.java
@@ -6,10 +6,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,6 +14,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/item")
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class ItemController {
 
     private final ItemService itemService;
