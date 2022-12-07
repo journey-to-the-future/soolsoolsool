ebce0da4260c2d891ccdd635867306723ea2c77c
Koojiny <msj09252@gmail.com>
2022-12-08 00:04:02 +0900

    dev : Modified settings & securityConfig

--- src/main/java/com/journey/web/config/SecurityConfig.java
+++ src/main/java/com/journey/web/config/SecurityConfig.java
@@ -31,52 +31,50 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        // CSRF 설정 Disable
-//        http.csrf().disable()
-//                .httpBasic().disable()
-//                //   .addFilter(corsConfig.corsFilter())
-//                .cors().configurationSource(corsConfigurationSource())
-//                .and()
-//                // exception handling 할 때 우리가 만든 클래스를 추가
-//                .exceptionHandling()
-//                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-//                .accessDeniedHandler(jwtAccessDeniedHandler)
-//                // 시큐리티는 기본적으로 세션을 사용
-//                // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
-//                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//
-//                // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/api/admin/**").hasAnyRole("ADMIN")
-//                .antMatchers("/api/member/**", "/api/conference/**", "/api/friend/**", "/api/emoji/**"
-//                        , "/api/emojishop/**", "/api/game/**", "/api/deal/**", "/api/point/**").authenticated()
-//                .antMatchers("/**").permitAll()
-//
-//                // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
-//                .and()
-//                .apply(new JwtSecurityConfig(tokenProvider));
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // CSRF 설정 Disable
+        http.csrf().disable()
+                .httpBasic().disable()
+                //   .addFilter(corsConfig.corsFilter())
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
+                // exception handling 할 때 우리가 만든 클래스를 추가
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                // 시큐리티는 기본적으로 세션을 사용
+                // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//
-//        CorsConfiguration configuration = new CorsConfiguration();
-//
-//        //    configuration.addAllowedOrigin("*");
-//        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5442", "https://i7a408.p.ssafy.io",
-//                "https://i7a408.p.ssafy.io:5442"));
-//        configuration.addAllowedHeader("*");
-//        configuration.addAllowedMethod("*");
-//        configuration.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
+                // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/admin/**").hasAnyRole("ADMIN")
+//                .antMatchers("/api/member/**", "/api/item/**").authenticated()
+                .antMatchers("/**").permitAll()
+
+                // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        //    configuration.addAllowedOrigin("*");
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5442"));
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
--- src/main/java/com/journey/web/config/cors/CorsConfig.java
+++ src/main/java/com/journey/web/config/cors/CorsConfig.java
@@ -1,14 +1,14 @@
-//package com.journey.web.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//
-//@Configuration
-//public class CorsConfig {
-//
+package com.journey.web.config.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
 //    @Bean
 //    public CorsFilter corsFilter() {
 //        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -21,4 +21,4 @@
 //        source.registerCorsConfiguration("/**", config);
 //        return new CorsFilter(source);
 //    }
-//}
+}
